4ae867267765c8ab2fa7e26be07deec2dd132cf7
hensb <git@hschoeneberg>
2013-03-20 13:19:48 +0100

    Bla

---
+++ src/de/info2/ub/auftragsformular/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package de.info2.ub.auftragsformular.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
